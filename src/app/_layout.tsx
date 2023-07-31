d990e550d78dd835fd1c13dfce6d1d466827ed2b
eder0782 <eder0782@gmail.com>
2023-07-31 18:21:56 -0400

    feat: add themeProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,75 +4,80 @@ import { SafeAreaView, Platform } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { useState, useEffect } from "react";
-
+import { MyTheme } from "../stylesBasics/theme";
+import { ThemeProvider } from "styled-components/native";
 
 export default function AppLayout() {
- 
   return (
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: "center", backgroundColor: "#ECF0F1" }}>   
-   
-      <StatusBar backgroundColor={"#242A32"} style="light" />
-      
-      <Tabs
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#242A32",
-          
-          },
-
-          headerTitleStyle: {
-            color: "#fff",
-          },
-          tabBarStyle: {
-            backgroundColor: "#242A32",
-            borderTopColor: "#0296E5",
-          },
-          tabBarActiveTintColor: "#0296E5",
-          tabBarInactiveTintColor: "#67686D",
+    <ThemeProvider theme={MyTheme}>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          backgroundColor: "#ECF0F1",
         }}
       >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "Inicio",
-            headerTitle: "O que você quer assistir hoje?",
-            tabBarLabelStyle: {
-              fontSize: 15,
+        <StatusBar backgroundColor={"#242A32"} style="light" />
+
+        <Tabs
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#242A32",
             },
 
-            tabBarIcon: ({ size, color }) => (
-              <MaterialIcons name="home" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="search/index"
-          options={{
-            title: "Pesquisa",
-
-            tabBarLabelStyle: {
-              fontSize: 15,
+            headerTitleStyle: {
+              color: "#fff",
             },
-            tabBarIcon: ({ size, color }) => (
-              <MaterialIcons name="search" size={size} color={color} />
-            ),
-          }}
-        />
-
-        <Tabs.Screen
-          name="favorite/index"
-          options={{
-            title: "Favoritos",
-            tabBarLabelStyle: {
-              fontSize: 15,
+            tabBarStyle: {
+              backgroundColor: "#242A32",
+              borderTopColor: "#0296E5",
             },
-            tabBarIcon: ({ size, color }) => (
-              <MaterialIcons name="flag" size={size} color={color} />
-            ),
+            tabBarActiveTintColor: "#0296E5",
+            tabBarInactiveTintColor: "#67686D",
           }}
-        />
-      </Tabs>
-    </SafeAreaView>
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Inicio",
+              headerTitle: "O que você quer assistir hoje?",
+              tabBarLabelStyle: {
+                fontSize: 15,
+              },
+
+              tabBarIcon: ({ size, color }) => (
+                <MaterialIcons name="home" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="search/index"
+            options={{
+              title: "Pesquisa",
+
+              tabBarLabelStyle: {
+                fontSize: 15,
+              },
+              tabBarIcon: ({ size, color }) => (
+                <MaterialIcons name="search" size={size} color={color} />
+              ),
+            }}
+          />
+
+          <Tabs.Screen
+            name="favorite/index"
+            options={{
+              title: "Favoritos",
+              tabBarLabelStyle: {
+                fontSize: 15,
+              },
+              tabBarIcon: ({ size, color }) => (
+                <MaterialIcons name="flag" size={size} color={color} />
+              ),
+            }}
+          />
+        </Tabs>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
