07085d2b577920d9819a73fe19df848879a19d8c
eder0782 <eder0782@gmail.com>
2023-08-04 15:15:17 -0400

    feat: header page Details Create

--- src/components/Details/HeaderDetails/HeaderDetails.style.tsx
+++ src/components/Details/HeaderDetails/HeaderDetails.style.tsx
@@ -1,9 +1,78 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  /* background-color: red; */
+  /* height: 350px; */
+`;
 
 export const BannerMovie = styled.Image`
-  borderradius: 0 0 16 16;
-  height: 210;
+  /* margin-top: 30px; */
+  border-radius: 0px 0px 16px 16px;
+  height: 240px;
   width: 100%;
+`;
+
+export const PosterMovie = styled.Image`
+  height: 155px;
+  width: 110px;
+  border-radius: 16px;
+  /* transform: translate(-100%, 50%); */
+  transform: translateY(-70px);
+  margin-left: 30px;
+  /* transform: translateX(-50px); */
+`;
+
+export const TitleMovie = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  color: #fff;
+  padding-left: 40%;
+  padding-right: 10%;
+`;
+
+export const Rating = styled.View`
+  background-color: #252836;
+  height: 34px;
+  width: 64px;
+  opacity: 0.9;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(-200px);
+  margin-left: 340px;
+  flex-direction: row;
+`;
+
+export const TextRating = styled.Text`
+  font-size: 18px;
+  color: #ff8700;
+  font-weight: bold;
+`;
+
+export const Icons = styled.Image`
+  height: 18px;
+  width: 18px;
+  margin-right: 5px;
+`;
+
+export const TextDescription = styled.Text`
+  color: #92929d;
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+export const ContainerDescription = styled.View`
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 40px;
+  flex-direction: row;
+`;
+
+export const ContainerDescriptionUnit = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  margin-right: 10px;
+  margin-left: 10px;
 `;
--- src/components/Details/HeaderDetails/index.tsx
+++ src/components/Details/HeaderDetails/index.tsx
@@ -1,10 +1,49 @@
 import React from "react";
-import { Container, BannerMovie } from "./HeaderDetails.style";
+import {
+  Container,
+  BannerMovie,
+  ContainerPoster,
+  PosterMovie,
+  TitleMovie,
+  Rating,
+  TextRating,
+  Icons,
+  ContainerDescription,
+  TextDescription,
+  ContainerDescriptionUnit,
+} from "./HeaderDetails.style";
+import { Title } from "../../../styles";
+import { View } from "react-native";
 
 const HeaderDetails: React.FC = () => {
   return (
     <Container>
-      <BannerMovie source={require("./image.png")} />
+      <View style={{ height: 260 }}>
+        <BannerMovie source={require("./image.png")} />
+
+        <PosterMovie source={require("./poster.png")} />
+        <Rating>
+          <Icons source={require("../../../assets/star.png")} />
+          <TextRating>9.5</TextRating>
+        </Rating>
+      </View>
+      <TitleMovie>Spiderman No Way Home</TitleMovie>
+      <ContainerDescription>
+        <ContainerDescriptionUnit>
+          <Icons source={require("../../../assets/calendar.png")} />
+          <TextDescription>2021</TextDescription>
+        </ContainerDescriptionUnit>
+        <TextDescription>|</TextDescription>
+        <ContainerDescriptionUnit>
+          <Icons source={require("../../../assets/clock.png")} />
+          <TextDescription>148 Minutos</TextDescription>
+        </ContainerDescriptionUnit>
+        <TextDescription>|</TextDescription>
+        <ContainerDescriptionUnit>
+          <Icons source={require("../../../assets/ticket.png")} />
+          <TextDescription>Ação</TextDescription>
+        </ContainerDescriptionUnit>
+      </ContainerDescription>
     </Container>
   );
 };
